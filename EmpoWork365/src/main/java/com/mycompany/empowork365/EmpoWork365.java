47024a2e676252ad430af0b3e79e472fa04271bd
mirafuentesjenalyn <mirafuentesjenalyn@gmail.com>
2024-10-08 16:20:11 +0800

    "EmpoWork365"

---
+++ EmpoWork365/src/main/java/com/mycompany/empowork365/EmpoWork365.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.empowork365;
+
+/**
+ *
+ * @author jenal
+ */
+public class EmpoWork365 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
